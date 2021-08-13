3bf1f7ae6fce14d3a4b1be0098b3fd94f9424ecf
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-08-13 11:26:42 +0300

    Completed Challenge

--- SelfieShare/ViewController.swift
+++ SelfieShare/ViewController.swift
@@ -19,6 +19,7 @@ final class ViewController: UICollectionViewController {
 
 	private enum Constants {
 		static let serviceType = "ic-selfieShare"
+		static let textMessage = "textMessage"
 	}
 
 	// MARK: - Lifecycle
@@ -26,11 +27,48 @@ final class ViewController: UICollectionViewController {
 		super.viewDidLoad()
 
 		title = "Selfie Share"
-		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
-		navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
+
+		let connectButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
+		let peersButton = UIBarButtonItem(image: UIImage(systemName: "person"), style: .plain, target: self, action: #selector(showPeersDidTap))
+		navigationItem.leftBarButtonItems = [connectButton, peersButton]
+
+		let addPictureButton = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+		let sendTextButton = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(sendTextDidTap))
+		navigationItem.rightBarButtonItems = [addPictureButton, sendTextButton]
 
 		mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
 		mcSession?.delegate = self
+	}
+
+	// MARK: - Text Sender
+	@objc private func sendTextDidTap() {
+		let textAlert = UIAlertController(title: "Send text", message: nil, preferredStyle: .alert)
+		textAlert.addTextField()
+		textAlert.addAction(UIAlertAction(title: "Done", style: .default) { [weak self] _ in
+			self?.send(text: textAlert.textFields?.first?.text)
+		})
+		textAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+		present(textAlert, animated: UIView.areAnimationsEnabled)
+	}
+
+	private func send(text: String?) {
+		guard let text = text else { return }
+		let myPeerID = mcSession?.myPeerID.displayName ?? "unknown user"
+		let textToSend = "\(Constants.textMessage);\(myPeerID);\(text)"
+		let textData = Data(textToSend.utf8)
+		send(data: textData)
+	}
+
+	// MARK: - Data Sender
+	private func send(data: Data) {
+		guard let session = mcSession, !session.connectedPeers.isEmpty else { return }
+		do {
+			try session.send(data, toPeers: session.connectedPeers, with: .reliable)
+		} catch {
+			let alertController = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
+			alertController.addAction(UIAlertAction(title: "OK", style: .default))
+			present(alertController, animated: UIView.areAnimationsEnabled)
+		}
 	}
 
 	// MARK: - Image Picker
@@ -38,7 +76,7 @@ final class ViewController: UICollectionViewController {
 		let picker = UIImagePickerController()
 		picker.allowsEditing = true
 		picker.delegate = self
-		present(picker, animated: true)
+		present(picker, animated: UIView.areAnimationsEnabled)
 	}
 
 	// MARK: - Sessions
@@ -47,7 +85,7 @@ final class ViewController: UICollectionViewController {
 		alertController.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
 		alertController.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
 		alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-		present(alertController, animated: true)
+		present(alertController, animated: UIView.areAnimationsEnabled)
 	}
 
 	private func startHosting(action: UIAlertAction) {
@@ -60,7 +98,30 @@ final class ViewController: UICollectionViewController {
 		guard let mcSession = mcSession else { return }
 		let mcBrowser = MCBrowserViewController(serviceType: Constants.serviceType, session: mcSession)
 		mcBrowser.delegate = self
-		present(mcBrowser, animated: true)
+		present(mcBrowser, animated: UIView.areAnimationsEnabled)
+	}
+
+	@objc private func showPeersDidTap() {
+		guard let mcSession = mcSession else { return }
+		let connectedPeers = mcSession.connectedPeers.map { peer -> String in
+			peer.displayName
+		}
+
+		guard !connectedPeers.isEmpty else {
+			let alertController = UIAlertController(title: "No peers", message: "There are no peers connected", preferredStyle: .alert)
+			alertController.addAction(UIAlertAction(title: "OK", style: .default))
+			present(alertController, animated: UIView.areAnimationsEnabled)
+			return
+		}
+		let alertController = UIAlertController(title: "Connected peers", message: connectedPeers.joined(separator: ", "), preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "OK", style: .default))
+		present(alertController, animated: UIView.areAnimationsEnabled)
+	}
+
+	private func showHasDisconnectedAlert(peerDisplayName: String) {
+		let alertController = UIAlertController(title: "Session ended", message: "\(peerDisplayName) has disconnected", preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "OK", style: .default))
+		present(alertController, animated: UIView.areAnimationsEnabled)
 	}
 }
 
@@ -91,20 +152,13 @@ extension ViewController: UIImagePickerControllerDelegate {
 	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
 		guard let image = info[.editedImage] as? UIImage else { return }
 
-		dismiss(animated: true)
+		dismiss(animated: UIView.areAnimationsEnabled)
 
 		images.insert(image, at: 0)
 		collectionView.reloadData()
 
-		guard let mcSession = mcSession, !mcSession.connectedPeers.isEmpty, let imageData = image.pngData() else { return }
-
-		do {
-			try mcSession.send(imageData, toPeers: mcSession.connectedPeers, with: .reliable)
-		} catch {
-			let alertController = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
-			alertController.addAction(UIAlertAction(title: "OK", style: .default))
-			present(alertController, animated: true)
-		}
+		guard let imageData = image.pngData() else { return }
+		send(data: imageData)
 	}
 }
 
@@ -116,19 +170,27 @@ extension ViewController: MCSessionDelegate {
 			if let image = UIImage(data: data) {
 				self?.images.insert(image, at: 0)
 				self?.collectionView.reloadData()
+			} else {
+				let text = String(decoding: data, as: UTF8.self)
+				var splittedText = text.split(separator: ";")
+				guard splittedText.removeFirst().hasPrefix(Constants.textMessage),
+					  let sender = splittedText.first,
+					  let message = splittedText.last else { return }
+				let alertController = UIAlertController(title: "Message from \(sender)", message: "\(message)", preferredStyle: .alert)
+				alertController.addAction(UIAlertAction(title: "OK", style: .default))
+				self?.present(alertController, animated: UIView.areAnimationsEnabled)
 			}
 		}
 	}
 
 	func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-		let stateName: String
 		switch state {
-		case .connected: stateName = "Connected"
-		case .connecting: stateName = "Connecting"
-		case .notConnected: stateName = "Not connected"
-		@unknown default: stateName = "Unknown state received"
+		case .connected: print("Connected: \(peerID.displayName)")
+		case .connecting: print("Connecting: \(peerID.displayName)")
+		case .notConnected: showHasDisconnectedAlert(peerDisplayName: peerID.displayName)
+		@unknown default: print("Unknown state: \(peerID.displayName)")
 		}
-		print("\(stateName): \(peerID.displayName)")
+
 	}
 
 	func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {}
@@ -142,10 +204,10 @@ extension ViewController: MCSessionDelegate {
 extension ViewController: MCBrowserViewControllerDelegate {
 
 	func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-		dismiss(animated: true)
+		dismiss(animated: UIView.areAnimationsEnabled)
 	}
 
 	func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-		dismiss(animated: true)
+		dismiss(animated: UIView.areAnimationsEnabled)
 	}
 }
